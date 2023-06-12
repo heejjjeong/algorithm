2eff4b843f5b9c826aea5f7d259377605e7f92d2
heejeong <71823666+heejjjeong@users.noreply.github.com>
2023-06-12 09:30:27 +0900

    43번 다시품

--- dev_cpp_inf/VS_AA/VS_AA/43.cpp
+++ dev_cpp_inf/VS_AA/VS_AA/43.cpp
@@ -8,6 +8,30 @@ using namespace std;
 
 #define TEST
 
+vector<int> vec;
+int n;
+
+int Count(int s)
+{
+	//들어온 s까지 몇 개의 테이프가 필요한지 계산하는 함수
+	int cnt = 1, sum = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		if (s < sum + vec[i])
+		{
+			++cnt;
+			sum = vec[i];
+		}
+		else
+		{
+			sum += vec[i];
+		}
+	}
+
+	return cnt;
+
+}
+
 int main()
 {
 #ifdef TEST
@@ -33,6 +57,33 @@ int main()
 	
 	*/
 
+	int m, lt = 1, rt = 0, mid, res = 0, temp, max = -2147000000;
+	cin >> n >> m;
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> temp;
+		vec.push_back(temp);
+		rt += temp;
+
+		if (max < vec[i])
+			max = vec[i];
+	}
+
+	while (lt <= rt)
+	{
+		mid = (rt + lt) / 2;
+		if (max <= mid  && Count(mid) <= m)
+		{
+			res = mid;
+			rt = mid - 1;
+		}
+		else
+		{
+			lt = mid + 1;
+		}
+	}
+	cout << res;
+/*
 	int n, m, temp, lt, rt, mid, sum = 0, sumTemp = 0, cnt = 0, res = 0;
 	cin >> n >> m;
 	vector<int> vec(n);
@@ -43,7 +94,6 @@ int main()
 		sum += temp;
 	}
 
-	/*
 	lt = 1;
 	rt = sum;
 	mid = (rt + lt) / 2;
@@ -62,7 +112,7 @@ int main()
 				++cnt;
 				sumTemp = vec[i];
 			}
-			else if (sumTemp == mid) //누적해서 더한 수가 mid보다 크다면
+			else if (sumTemp == mid)
 			{
 				++cnt;
 				sumTemp = 0;

99c58fa1b138fe2927e09a38ab10a73b9c06d24e
heejeong <71823666+heejjjeong@users.noreply.github.com>
2023-07-08 00:06:03 +0900

    52코드정리

--- dev_cpp_inf/VS_AA/VS_AA/52.cpp
+++ dev_cpp_inf/VS_AA/VS_AA/52.cpp
@@ -17,19 +17,35 @@ int main()
 
 	freopen_s(&stream, "input.txt", "r", stdin);
 #endif
-	int n, p2 = 1, p3 = 1, p5 = 1, temp = 1;
+	int n, p2 = 1, p3 = 1, p5 = 1, min = 1, temp = 1;
 	vector<int> vec(1501,0);
 	cin >> n;
 
 	for (int i = 1; i <= n; ++i)
 	{
-		vec[i] = temp;
+		vec[i] = min;
 
 		int numP2 = vec[p2] * 2;
 		int numP3 = vec[p3] * 3;
 		int numP5 = vec[p5] * 5;
+		
+		//그냥 최솟값 찾아서 걔랑 같은애 ++를 시켜주면 되는건데...
+		if(numP2 < numP3)
+			min = vec[p2] * 2;
+		else 
+			min = vec[p3] * 3;
 
-		if (numP2 < numP3 && numP2 < numP5)
+		if(numP5 < min)
+			min = vec[p5] * 5;
+
+		if (min == vec[p2] * 2)
+			p2++;
+		if (min == vec[p3] * 3)
+			p3++;
+		if (min == vec[p5] * 5)
+			p5++;
+
+		/*if (numP2 < numP3 && numP2 < numP5)
 		{
 			temp = vec[p2++] * 2;
 			continue;
@@ -69,7 +85,7 @@ int main()
 			temp = vec[p3++] * 3;
 			p5++;
 			continue;
-		}
+		}*/
 	}
 	cout << vec[n];
 	return 0;

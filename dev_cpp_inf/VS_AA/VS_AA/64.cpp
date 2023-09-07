f48576f27e0f92842a5f3e219a2abadba22dde0c
heejeong <71823666+heejjjeong@users.noreply.github.com>
2023-09-08 07:53:37 +0900

    66품

--- dev_cpp_inf/VS_AA/VS_AA/64.cpp
+++ dev_cpp_inf/VS_AA/VS_AA/64.cpp
@@ -1,65 +1,45 @@
-#include <stdio.h>
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-struct Vertex
-{
-	//int data;
-};
-
-int n, m, tempN, tempM, cnt = 0;
-
-
-void Dfs(int here, const vector<Vertex> Verties, const vector<vector<int>> Adjacent, vector<bool> Visited)
-{
-	if (Visited[here])
-		return;
-
-	Visited[here] = true;
-
-	if ( n == here )
-	{
-		/*for (int i = 1; i < Visited.size(); ++i)
-		{
-			if (Visited[i])
-				cout << i << " ";
+int map[30][30], ch[30], cnt = 0, n;
+void DFS(int v) {
+	int i;
+	if (v == n) {
+		cnt++;
+	}
+	else {
+		for (i = 1; i <= n; i++) {
+			if (map[v][i] == 1 && ch[i] == 0) {
+				ch[i] = 1;
+				DFS(i);
+				ch[i] = 0;
+			}
 		}
-		cout << endl;*/
-		++cnt;
-		return;
 	}
-
-	for (int i = 0; i < Adjacent[here].size(); ++i)
-	{
-		int there = Adjacent[here][i];
-		Dfs(there, Verties, Adjacent, Visited);
-	}
-
 }
 
-//#define TEST
+
+#define TEST
 int main()
 {
 #ifdef TEST
 	FILE* stream;
 	freopen_s(&stream, "input.txt", "r", stdin);
 #endif
+	int m, a, b;
+
 	std::cin >> n >> m;
-	vector<Vertex> Verties(n + 1);
-	vector<vector<int>> Adjacent(n + 1);
-	vector<bool> Visited(n + 1, false);
 
-	for (int i = 1; i < m + 1; ++i)
-	{
-		cin >> tempN >> tempM;
-		Adjacent[tempN].push_back(tempM);
-	};
-	
-	Dfs(1, Verties, Adjacent, Visited);
-
+	for (int i = 1; i <= m; i++) {
+		cin >> a >> b;
+		map[a][b] = 1;
+	}
+	ch[1] = 1;
+	DFS(1);
 	cout << cnt;
 
 	return 0;
 }
+
